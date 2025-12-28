78c094eacf993db82b866e91198f60f43c89a661
Berny70 <37933006+Berny70@users.noreply.github.com>
2025-12-28 21:16:38 +0100

    Create service-worker.js

---
+++ v2/service-worker.js
@@ -0,0 +1,47 @@
+const APP_VERSION = "1.0";
+const CACHE = "chrono-pwa-v1.0";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+self.addEventListener("message", event => {
+  if (event.data === "GET_VERSION") {
+    event.source.postMessage({ version: APP_VERSION });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
