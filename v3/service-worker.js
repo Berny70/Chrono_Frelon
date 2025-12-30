578161d6c36d76c20a7170a232f7efba6d2e6456
Berny70 <37933006+Berny70@users.noreply.github.com>
2025-12-30 10:33:46 +0100

    Update service-worker.js

--- v3/service-worker.js
+++ v3/service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "3.0";
-const CACHE = "chrono-pwa-v3.0";
+const APP_VERSION = "3.1";
+const CACHE = "chrono-pwa-v3.1";
 const FILES = [
   "./",
   "./index.html",
@@ -25,6 +25,7 @@ self.addEventListener("message", event => {
     event.source.postMessage({ version: APP_VERSION });
   }
 });
+
 
 
 
