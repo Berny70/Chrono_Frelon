0faaf343e64fb9a248442c079970f9114e624e65
Berny70 <37933006+Berny70@users.noreply.github.com>
2026-01-17 11:14:24 +0100

    Update service-worker.js

--- v4/service-worker.js
+++ v4/service-worker.js
@@ -1,17 +1,31 @@
-const APP_VERSION = "4.1";
-const CACHE = "chrono-pwa-v4.1";
+const APP_VERSION = "4.2";
+const CACHE = "chrono-pwa-v4.2";
+
 const FILES = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
+  "./version.js",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -27,48 +41,3 @@ self.addEventListener("message", event => {
     event.source.postMessage({ version: APP_VERSION });
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
