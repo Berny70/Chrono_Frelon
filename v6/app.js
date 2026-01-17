057fbaa4c2b6da71975bc6d245c0812f13aa489e
Berny70 <37933006+Berny70@users.noreply.github.com>
2026-01-17 08:36:51 +0100

    Update app.js

--- v6/app.js
+++ v6/app.js
@@ -1,1 +1,8 @@
-
+export const state = {
+  chronos: [
+    { color:"red", essais:[], directions:[], lat:null, lon:null, vitesse:4 },
+    { color:"blue", essais:[], directions:[], lat:null, lon:null, vitesse:4 },
+    { color:"green", essais:[], directions:[], lat:null, lon:null, vitesse:4 },
+    { color:"white", essais:[], directions:[], lat:null, lon:null, vitesse:4 }
+  ]
+};
