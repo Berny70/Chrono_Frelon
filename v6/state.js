94af05119d6b5a1731dd5e4bf111f041e008112d
Berny70 <37933006+Berny70@users.noreply.github.com>
2026-01-17 08:39:15 +0100

    Create state.js

---
+++ v6/state.js
@@ -0,0 +1,53 @@
+// state.js
+// ==========================
+// ÉTAT CENTRAL – V6
+// ==========================
+
+export const state = {
+  chronos: [
+    {
+      color: "red",
+      running: false,
+      startTime: 0,
+      essais: [],
+      directions: [],
+      direction: 0,
+      vitesse: 4,
+      lat: null,
+      lon: null
+    },
+    {
+      color: "blue",
+      running: false,
+      startTime: 0,
+      essais: [],
+      directions: [],
+      direction: 0,
+      vitesse: 4,
+      lat: null,
+      lon: null
+    },
+    {
+      color: "green",
+      running: false,
+      startTime: 0,
+      essais: [],
+      directions: [],
+      direction: 0,
+      vitesse: 4,
+      lat: null,
+      lon: null
+    },
+    {
+      color: "white",
+      running: false,
+      startTime: 0,
+      essais: [],
+      directions: [],
+      direction: 0,
+      vitesse: 4,
+      lat: null,
+      lon: null
+    }
+  ]
+};
