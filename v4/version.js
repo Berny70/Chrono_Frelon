366d927fbc1e32f52d5ebd8bef766c79a9f2c916
Berny70 <37933006+Berny70@users.noreply.github.com>
2026-01-18 10:04:54 +0100

    Update version.js

--- v4/version.js
+++ v4/version.js
@@ -1,5 +1,5 @@
 // version.js
 // Numéro de version de l'application Chronos v2
 // version.js
-window.APP_VERSION = window.APP_VERSION || "4.9";
+window.APP_VERSION = window.APP_VERSION || "4.10";
 
