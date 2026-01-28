91a8b522ca2a25ca35708142d942be7637b85994
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-01-28 08:33:48 +0100

    Update service-worker.js

--- v8/service-worker.js
+++ v8/service-worker.js
@@ -2,8 +2,8 @@
    SERVICE WORKER – Chrono Frelon
    ========================== */
 
-const APP_VERSION = "9.10";
-const CACHE_NAME = "chrono-frelon-v9.10";
+const APP_VERSION = "9.11";
+const CACHE_NAME = "chrono-frelon-v9.11";
 
 /* ⚠️ Liste STRICTE des fichiers à mettre en cache
    (éviter "./" qui peut matcher trop large) */
@@ -96,6 +96,7 @@ self.addEventListener("message", event => {
     });
   }
 });
+
 
 
 
