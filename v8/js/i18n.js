5bca08e2b2d20bfb59ab8ac8977a72204b34dd34
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-01-28 10:00:41 +0100

    Update i18n.js

--- v8/js/i18n.js
+++ v8/js/i18n.js
@@ -3,7 +3,7 @@
 // ==========================
 let LANG = "fr";
 let STRINGS = {};
-const SUPPORTED_LANGS = ["fr", "en", "de", "it", "es", "als", "bz", "oc"];
+const SUPPORTED_LANGS = ["fr", "en", "de", "it", "es", "nl", "als", "bz", "oc", "ik"];
 
 // ==========================
 // CHARGEMENT LANGUE
