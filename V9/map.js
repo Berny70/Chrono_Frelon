5cd015af5e6d120e3c6ad0753b1c09666e7ec438
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-03-26 11:14:26 +0100

    Update map.js

--- V9/map.js
+++ V9/map.js
@@ -34,24 +34,27 @@ function loadDeclinaison() {
 // ==========================
 // INPUT DECLINAISON
 // ==========================
-function setupDeclinaisonInput() {
-  const input = document.getElementById("declinaisonInput");
-  if (!input) return;
-
-  input.addEventListener("input", () => {
-    let val = parseFloat(input.value);
-
-    if (isNaN(val)) return;
-
-    if (val > 30) val = 30;
-    if (val < -30) val = -30;
-
-    declinaison = val;
-    localStorage.setItem("declinaison", declinaison);
-
-    redrawMap();
-  });
-}
+    function setupDeclinaisonInput() {
+      const input = document.getElementById("declinaisonInput");
+      if (!input) return;
+    
+      input.addEventListener("change", () => {
+        let val = parseFloat(input.value.replace(",", "."));
+    
+        if (isNaN(val)) return;
+    
+        // limites
+        if (val > 30) val = 30;
+        if (val < -30) val = -30;
+    
+        declinaison = val;
+        localStorage.setItem("declinaison", declinaison);
+    
+        input.value = declinaison.toFixed(1);
+    
+        redrawMap();
+      });
+    }
 
 // ==========================
 // REFRESH CARTE
