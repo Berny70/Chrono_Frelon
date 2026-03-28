ab8b8c93d036e5971fba32e607638db401cdc41e
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-03-28 09:59:02 +0100

    Update service-worker.js

--- v9/service-worker.js
+++ v9/service-worker.js
@@ -2,8 +2,8 @@
    SERVICE WORKER – Chrono Frelon
    ========================== */
 
-const APP_VERSION = "11.86";
-const CACHE_NAME = "chrono-frelon-v11.86";
+const APP_VERSION = "12";
+const CACHE_NAME = "chrono-frelon-v12";
 
 /* ⚠️ Liste STRICTE des fichiers à mettre en cache
    (éviter "./" qui peut matcher trop large) */
