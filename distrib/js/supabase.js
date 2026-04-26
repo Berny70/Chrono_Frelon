2a834758889ba379968c704f02f3157b4f79870a
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-04-26 07:37:26 +0200

    Update supabase.js

--- distrib/js/supabase.js
+++ distrib/js/supabase.js
@@ -1,7 +1,9 @@
 // /js/supabase.js
+//    https://supabase.com/dashboard/project/pqozgsgytzntrqscevrt/settings/general
+//    Berny70_chrono
 
-const SUPABASE_URL = "https://nxrezfbzhhuxtnwhbjzz.supabase.co";
-const SUPABASE_KEY = "sb_publishable_pv2SCbULI-7GStKIuRmWFg_zbaC69KE";
+const SUPABASE_URL = "https://pqozgsgytzntrqscevrt.supabase.co";
+const SUPABASE_KEY = "sb_publishable_lPacACUx-QTq_-mV7DDc_g_xUQHDCsK";
 
 // Initialisation UNIQUE (globale)
 if (!window.supabaseClient) {
